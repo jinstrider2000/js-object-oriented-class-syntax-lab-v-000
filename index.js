ca3114a2f19b6f5093f5db6479745ea2ba7db3b6
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 15:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ class BoardMember {
     this.training = training;
   }
 
-  function veto() {
+  veto() {
     return 'No, I must disagree';
   }
 
-  function approve() {
+  approve() {
     return 'You can do that!';
   }
 
-  function doCharity() {
+  doCharity() {
     return "I like to help people.";
   }
 
-  function releasePressStatement() {
+  releasePressStatement() {
     return "You will see great things from Scuber.";
   }
 
-  function sayHi() {
+  sayHi() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
 }
@@ -31,7 +31,7 @@ class Ceo extends BoardMember{
     super.constructor();
   }
 
-  function hireEmployee() {
+  hireEmployee() {
     return "Welcome aboard!";
   }
 }
